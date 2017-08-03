ae19e4ddb08dbdcf5544848efb79e1a346eb0e93
austinChappell <Austin.W.Chappell@gmail.com>
2017-08-03 09:25:42 -0500

    add comments

--- routes/router.js
+++ routes/router.js
@@ -6,40 +6,53 @@ let postData = {};
 
 router.get('/', (req, res) => {
 
+  // GET A NEW WORD
   function getNewWord(min, max) {
+    // CREATE A NEW WORD, CONVERT TO UPPERCSE, SPLIT TO AN ARRAY
     req.session.word = randomWords().toUpperCase().split('');
+    // RUN RECURSIVELY UNTIL PARAMETERS ARE TRUE
     if (req.session.word.length < min || req.session.word.length > max) {
       getNewWord(min, max);
     }
+    // USED TO INCREMENT THE USERS SCORE
     req.session.scoreDivider = req.session.word.length;
+    // INITIAL USER SCORE
     req.session.score = 0;
   }
 
+  // IF DIFFICULTY HAS NOT BEEN CHOSEN, THE GAME HAS NOT BEGUN
   if (req.session.difficultyChosen === false) {
     req.session.beginGame = false;
   };
 
+  // IF THE GAME HAS BEGUN, DO THIS
   if (req.session.beginGame === true) {
     if (req.session.difficulty === 'easy') {
+      // IF DIFF. IS EASY, GET A WORD BETWEEN 4 AND 6 LETTERS
       getNewWord(4, 6);
+      // MAX SCORE FOR EASY IS 1500
       req.session.maxScore = 1500;
     } else if (req.session.difficulty === 'medium') {
+      // IF DIFF. IS MEDIUM, GET A WORD BETWEEN 6 AND 8 LETTERS
       getNewWord(6, 8);
+      // MAX SCORE FOR MEDIUM IS 2000
       req.session.maxScore = 2000;
     } else {
+      // IF DIFF. IS HARD, GET A WORD OF 8 LETTERS OR MORE
       getNewWord(8);
+      // MAX SCORE FOR HARD IS 2500
       req.session.maxScore = 2500;
     }
+    // BEGIN GAME IS FALSE SO THIS FUNCTION DOES NOT CONTINUE TO RUN
     req.session.beginGame = false;
     req.session.playing = true;
     req.session.didWin = false;
     req.session.blankArr = [];
+    // CREATE UNDERSCORES EQUAL IN NUMBER TO THE NUMBER OF LETTERS IN THE WORD
     req.session.word.forEach(function(letter) {
       req.session.blankArr.push('_');
     });
     req.session.blanksRemaining = req.session.blankArr.length;
-    req.session.correctCount = 0;
-    req.session.incorrectCount = 0;
     req.session.guesses = [];
     req.session.guessesRemaining = 8;
     req.session.alreadyGuessed = false;
@@ -51,14 +64,18 @@ router.get('/', (req, res) => {
     req.session.word = '';
   }
 
+  // IF ALL OF THE LETTERS IN THE WORD HAVE BEEN GUESSED
   if (req.session.blanksRemaining === 0) {
+    // ROUND THE USER SCORE
     let score = Math.round(req.session.score);
     req.session.gameOverMsg = `You win! Your score is ${ score }. Do you want to play again or add your score to the leader board?`;
     req.session.didWin = true;
     clearGame();
   }
 
+  // IF THE USER IS OUT OF GUESSES
   if (req.session.guessesRemaining === 0) {
+    // JOIN THE ARRAY INTO A STRING
     let answer = req.session.word.join('');
     req.session.gameOverMsg = `You lose. The word was ${ answer }.`;
     clearGame();
@@ -112,10 +129,7 @@ router.post('/guess', (req, res) => {
             }
           };
 
-          if (isCorrect) {
-            req.session.correctCount++;
-          } else {
-            req.session.incorrectCount++;
+          if (!isCorrect) {
             req.session.guessesRemaining--;
             req.session.scoreDivider++;
           }
